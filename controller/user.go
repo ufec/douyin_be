8011747d786b6b94f1fc0fd793f419d6c9895688
ufec <1750367868@qq.com>
2022-06-07 11:03:35 +0800

    fix: fix getUserInfo

--- controller/user.go
+++ controller/user.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/ufec/douyin_be/model"
 	"net/http"
+	"strconv"
 )
 
 // UsersLoginInfo use map to store user info, and key is username+password for demo
@@ -74,42 +75,39 @@ func Login(c *gin.Context) {
 }
 
 func UserInfo(c *gin.Context) {
-	token := c.Query("token")
-	if user, exist := UsersLoginInfo[token]; exist {
-		c.JSON(http.StatusOK, UserResponse{
-			Response: Response{StatusCode: 0},
-			User: User{
-				Id:            int64(user.ID),
-				Name:          user.UserName,
-				FollowCount:   int64(user.FollowCount),
-				FollowerCount: int64(user.FollowerCount),
-				IsFollow:      false,
-			},
-		})
-	} else {
-		c.JSON(http.StatusOK, UserResponse{
-			Response: Response{StatusCode: 1, StatusMsg: "User doesn't exist"},
-		})
+	loginUserId, queryUserIdStr := c.GetUint("userID"), c.Query("user_id")
+	var queryUserId uint64
+	if queryUserIdStr != "" {
+		if _queryUserId, parseUintErr := strconv.ParseUint(queryUserIdStr, 10, 64); parseUintErr != nil {
+			Failed(c, parseUintErr.Error())
+			return
+		} else {
+			queryUserId = _queryUserId
+		}
 	}
-	//userId := c.GetUint("userID")
-	//// 默认值为0 主键ID不为0 则说明用户不存在
-	//if userId == 0 {
-	//	c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "不存在该用户"})
-	//	return
-	//}
-	//userInfo, getUserInfoErr := userService.GetUserInfoById(userId)
-	//if getUserInfoErr != nil {
-	//	c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: getUserInfoErr.Error()})
-	//	return
-	//}
-	//c.JSON(http.StatusOK, UserResponse{
-	//	Response: Response{StatusCode: 0},
-	//	User: User{
-	//		Id:            int64(userInfo.ID),
-	//		Name:          userInfo.UserName,
-	//		FollowCount:   int64(userInfo.FollowCount),
-	//		FollowerCount: int64(userInfo.FollowerCount),
-	//		IsFollow:      false,
-	//	},
-	//})
+	// 默认值为0 主键ID不为0 则说明用户不存在
+	if loginUserId == 0 {
+		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "不存在该用户"})
+		return
+	}
+	isFollow, userId := false, loginUserId // 默认查当前登录用户信息
+	if queryUserId != 0 {
+		userId = uint(queryUserId) // 传入的查询用户ID不为空则查传入的用户id信息
+		isFollow = IsFollow(loginUserId, uint(queryUserId))
+	}
+	userInfo, getUserInfoErr := userService.GetUserInfoById(userId)
+	if getUserInfoErr != nil {
+		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: getUserInfoErr.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, UserResponse{
+		Response: Response{StatusCode: 0},
+		User: User{
+			Id:            int64(userInfo.ID),
+			Name:          userInfo.UserName,
+			FollowCount:   int64(userInfo.FollowCount),
+			FollowerCount: int64(userInfo.FollowerCount),
+			IsFollow:      isFollow,
+		},
+	})
 }
